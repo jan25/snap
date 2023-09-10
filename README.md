6eba76d99d4e3bd7064917b722b8791e049b865c
Abhilash Gnan <abhilashgnan@gmail.com>
2023-09-10 20:22:28 +0100

    Provide options and colors

--- README.md
+++ README.md
@@ -8,61 +8,49 @@ TODO add curl command to download.
 
 ## Usage
 
-> TODO Some of features below are WIP.
-
-snap requires you to specify a target command to test, it then evaluates your test cases against the output from that command. To do this create snap.cmd file in the directory where you intend to put your test cases.
+Supplying test directory and command to test to snap will enumerate test case snapshots suffixed with `.output|.input` in the directory.
 
 ```sh
-# Example
-$ echo "python3 myawesomeprogram.py" >> snap.cmd
-```
-
-snap runs your tests from a directory. To run your snapshot tests create test cases suffixed with `.input` and `.output` in a directory.
-
-```sh
-$ snap.sh /path/to/test/dir
-Found 45 tests..
+$ sh snap.sh -t /tests/dir -c "cat FILE"
+Found 2 tests..
 PASS test1
 FAIL test2
-...
-
-40 PASS 5 FAIL
+TOTAL 2 PASS 1 FAIL 1 SKIP 0
 ```
 
-snap can automatically patch your test cases by passing an option.
+> Note: `FILE` is a special placeholder which us used to pipe inputs to the command under test.
+
+
+Use `-u|--update` to let snap update snaphot files for failing tests.
 
 ```sh
-$ snap.sh /path/to/test/dir --update
-Patching 5 tests
-DONE test1
-DONE test3
-...
-```
+$ sh snap.sh -t /tests/dir -c "cat FILE" -u
+Found 2 tests..
+PASS test1
+FAIL test2
+TOTAL 2 PASS 1 FAIL 1 SKIP 0
 
-By default snap runs all your tests. You can optionally tell it to run specific tests.
-
-```sh
-$ snap.sh /path/to/test/dir --tests test2,test4
-Running 2 tests
+$ sh snap.sh -t /tests/dir -c "cat FILE"
+Found 2 tests..
+PASS test1
 PASS test2
-...
+TOTAL 2 PASS 2 FAIL 0 SKIP 0
 ```
-
-snap can run multiple test concurrently. Default is serial execution.
+By default snap runs all your tests. You can optionally tell it to run specific tests with `-t|--tests` option.
 
 ```sh
-$ snap.sh /path/to/test/dir --parallel 3
-...
+$ sh snap.sh -t /tests/dir -c "cat FILE" -t "test1,test3"
+Running 1 tests..
+PASS test1
+TOTAL 1 PASS 1 FAIL 0 SKIP 0
 ```
-
-Use `--dry-run` option to print diff and not update snapshots.
 
 ## Develop
 
 `examples` directory contains sample test cases to test snap locally.
 
 ```sh
-$ snap.sh ./examples "cat FILE"
+$ snap.sh -t ./examples -c "cat FILE"
 ```
 
 Use [`shellcheck`](https://github.com/koalaman/shellcheck) to catch potential bugs and conform with bash standards.
--- snap.sh
+++ snap.sh
@@ -5,32 +5,31 @@ set -e
 TMPDIR=$(mktemp -d)
 trap 'rm -rf $TMPDIR' EXIT
 
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+ENDCOLOR="\033[0m"
+
 USAGE=$(cat << EOF
 Usage:
-    $ snap.sh /path/to/tests/dir "command to run" [optional arguments]
-    Optional arguments:
-    testList(string)    List of test names to filter for when running tests. Test names are delimited by comma character. By default snap runs all tests.
-    N(int)              Run N tests concurrently. Default is to run with N=1 concurrency.
+$ snap.sh -d /path/to/tests/dir -c "command to run" [-t "test1,test2" -p 3 -u -h]
+    
+    Required parameters:
+    -d|--directory      Directory to find test cases.
+    -c|--command        Command to test.
+    
+    Optional parameters: 
+    -t|--tests          Filter tests to run. By default runs all tests.
+    -p|--parallel       Run N tests in parallel. By default runs tests serially.
+    -u|--update         Update snapshot files for failing tests. 
+    -h|--help           Prints this help message.
 EOF
 )
 
 function error() {
-    echo "Error: $1\n$USAGE"
+    echo "${RED}Error: $1${ENDCOLOR}\n$USAGE"
     exit 1
 }
-
-[[ -n $1 ]] || error "test directory not provided."
-WORKDIR=$1
-test -d "$WORKDIR" || error "Invalid directory $WORKDIR"
-shift
-
-[[ -n $1 ]] || error "target program not provided"
-CMD=$1
-shift
-
-TESTS=$1
-
-PARALLEL=$2
 
 function testname() {
     path=$1
@@ -44,7 +43,7 @@ function runtest() {
 
     out=${inp/.input/.output}
     if [[ ! -f $out ]]; then
-        echo "SKIP $test"
+        echo "${YELLOW}SKIP${ENDCOLOR} $test"
         return 2
     fi
 
@@ -54,11 +53,11 @@ function runtest() {
 
     diff "$out" "${TMPDIR}/${test}.actual" > "${TMPDIR}/${test}.diff" && true
     if [[ $? -ne 0 ]]; then
-        patch -s "$out" "${TMPDIR}/${test}.diff"
-        echo "FAIL $test"
+        [[ "$UPDATE" -eq 1 ]] && patch -s "$out" "${TMPDIR}/${test}.diff"
+        echo "${RED}FAIL${ENDCOLOR} $test"
         return 1
     fi
-    echo "PASS $test"
+    echo "${GREEN}PASS${ENDCOLOR} $test"
 }
 
 function shouldrun() {
@@ -72,6 +71,39 @@ function shouldrun() {
     fi
     return 1
 }
+
+UPDATE=0
+
+while (( $# > 0 )); do
+    case "$1" in
+        -d|--directory)
+            WORKDIR=$2
+            [[ -n $WORKDIR ]] || error "test directory not provided."
+            test -d "$WORKDIR" || error "Invalid directory $WORKDIR"
+            shift; shift
+            ;;
+        -c|--command)
+            CMD=$2
+            [[ -n $CMD ]] || error "target program not provided"
+            shift; shift
+            ;;
+        -t|--tests)
+            TESTS=$2
+            shift; shift
+            ;;
+        -u|--update)
+            UPDATE=1
+            shift
+            ;;
+        -p|--parallel)
+            error "parallel not supported"
+            ;;
+        -h|--help|*)
+            echo "$USAGE"
+            exit 0
+            ;;
+    esac
+done
 
 TOTAL=0
 PASS=0
@@ -102,4 +134,7 @@ do
     fi
 done
 
-echo "TOTAL $TOTAL PASS $PASS FAIL $FAIL SKIP $SKIP"
+echo "TOTAL $TOTAL" \
+    "${GREEN}PASS $PASS${ENDCOLOR}" \
+    "${RED}FAIL $FAIL${ENDCOLOR}" \
+    "${YELLOW}SKIP $SKIP${ENDCOLOR}"
